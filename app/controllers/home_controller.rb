a59fa19afa24a287611a1c52ac182a634ff06e05
dmoore5050 <dustinmoore@gmail.com>
2014-04-22 09:32:03 -0500

    root now displays user icon/first name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	user = User.find_by_identifier session[:identifier]
-  	@descrip = get_user_full_description(user) 
+  	@descrip = get_user_full_description(current_user) if current_user
   end
 end
